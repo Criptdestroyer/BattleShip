46680c9700694b6e5ef4ad773df07a85e5799c17
Criptdestroyer <ahmademir1201@gmail.com>
2018-11-17 14:11:15 +0700

    rotas T dan I 4 done

--- src/battleship/BattleShip.java
+++ src/battleship/BattleShip.java
@@ -34,38 +34,51 @@ public class BattleShip {
 
     public void Generate() {
         //rotasi L
-        if (rand.nextInt() % 7 == 0) {
+        if (rand.nextInt() % 5 == 0) {
             L4();
-        } else if (rand.nextInt() % 5 == 0) {
-            L3();
         } else if (rand.nextInt() % 3 == 0) {
+            L3();
+        } else if (rand.nextInt() % 2 == 0) {
             L2();
         } else {
             L();
         }
-        
+
         //rotasi lurus
-        if(rand.nextInt()%7 == 0){
-            LR(4);
-        }
         if (rand.nextInt() % 5 == 0) {
-            LR3();
+            LR4();
         } else if (rand.nextInt() % 3 == 0) {
+            LR3();
+        } else if (rand.nextInt() % 2 == 0) {
             LR2();
-        } else{
+        } else {
             LR();
         }
         
-        if (rand.nextInt() % 2 == 0) {
-            T();
-        } else {
+        //rotasi T
+        if (rand.nextInt() % 5 == 0) {
+            T4();
+        } else if (rand.nextInt() % 3 == 0) {
+            T3();
+        } else if (rand.nextInt() % 2 == 0) {
             T2();
-        }
-        if (rand.nextInt() % 2 == 0) {
-            I();
         } else {
-            I2();
+            T();
         }
+        
+        //rotasi I
+        if (rand.nextInt() % 5 == 0) {
+            I4();
+        } else if (rand.nextInt() % 3 == 0) {
+            I3();
+        } else if (rand.nextInt() % 2 == 0) {
+            I2();
+        } else {
+            I();
+        }
+        
+
+        
     }
 
     private void L() {
@@ -249,11 +262,12 @@ public class BattleShip {
             }
         }
     }
+
     private void LR4() {
         boolean temp = true;
         Point s = new Point();
         while (temp) {
-            int y = rand.nextInt(6)+3;
+            int y = rand.nextInt(6) + 3;
             int x = rand.nextInt(6);
             LR[0] = new Point(x, y);
             for (int i = 1; i <= 3; i++) {
@@ -372,6 +386,86 @@ public class BattleShip {
         }
     }
 
+    private void T3() {
+        boolean temp = true;
+        Point s = new Point();
+        while (temp) {
+            int x = rand.nextInt(7) + 2;
+            int y = rand.nextInt(8);
+            T[0] = new Point(x, y);
+            for (int i = 1; i <= 3; i++) {
+                x--;
+                T[i] = new Point(x, y);
+                if (i == 1) {
+                    i++;
+                    y++;
+                    T[i] = new Point(x, y);
+                    y--;
+                }
+
+            }
+            int i = 0, j;
+            boolean sama = false;
+            while (i < T.length && sama == false) {
+                j = 0;
+                while (j < cek.size() && sama == false) {
+                    if (s.EQ(T[i], cek.get(j))) {
+                        sama = true;
+                    }
+                    j++;
+                }
+                i++;
+            }
+
+            if (sama == false) {
+                temp = false;
+                for (int k = 0; k < T.length; k++) {
+                    cek.add(T[k]);
+                }
+            }
+        }
+    }
+
+    private void T4() {
+        boolean temp = true;
+        Point s = new Point();
+        while (temp) {
+            int x = rand.nextInt(7) + 2;
+            int y = rand.nextInt(8) + 1;
+            T[0] = new Point(x, y);
+            for (int i = 1; i <= 3; i++) {
+                x--;
+                T[i] = new Point(x, y);
+                if (i == 1) {
+                    i++;
+                    y--;
+                    T[i] = new Point(x, y);
+                    y++;
+                }
+
+            }
+            int i = 0, j;
+            boolean sama = false;
+            while (i < T.length && sama == false) {
+                j = 0;
+                while (j < cek.size() && sama == false) {
+                    if (s.EQ(T[i], cek.get(j))) {
+                        sama = true;
+                    }
+                    j++;
+                }
+                i++;
+            }
+
+            if (sama == false) {
+                temp = false;
+                for (int k = 0; k < T.length; k++) {
+                    cek.add(T[k]);
+                }
+            }
+        }
+    }
+
     public void printT() {
         System.out.println("T");
         for (int i = 0; i < T.length; i++) {
@@ -420,6 +514,71 @@ public class BattleShip {
             int y = rand.nextInt(8);
             I[0] = new Point(x, y);
             y++;
+            I[1] = new Point(x, y);
+            int i = 0, j;
+            boolean sama = false;
+            while (i < I.length && sama == false) {
+                j = 0;
+                while (j < cek.size() && sama == false) {
+                    if (s.EQ(I[i], cek.get(j))) {
+                        sama = true;
+                    }
+                    j++;
+                }
+                i++;
+            }
+
+            if (sama == false) {
+                temp = false;
+                for (int k = 0; k < I.length; k++) {
+                    cek.add(I[k]);
+                }
+            }
+
+        }
+    }
+
+    private void I3() {
+        boolean temp = true;
+        Point s = new Point();
+        while (temp) {
+            int x = rand.nextInt(8);
+            int y = rand.nextInt(8);
+            I[0] = new Point(x, y);
+            y++;
+            x++;
+            I[1] = new Point(x, y);
+            int i = 0, j;
+            boolean sama = false;
+            while (i < I.length && sama == false) {
+                j = 0;
+                while (j < cek.size() && sama == false) {
+                    if (s.EQ(I[i], cek.get(j))) {
+                        sama = true;
+                    }
+                    j++;
+                }
+                i++;
+            }
+
+            if (sama == false) {
+                temp = false;
+                for (int k = 0; k < I.length; k++) {
+                    cek.add(I[k]);
+                }
+            }
+
+        }
+    }
+    private void I4() {
+        boolean temp = true;
+        Point s = new Point();
+        while (temp) {
+            int x = rand.nextInt(8);
+            int y = rand.nextInt(8)+1;
+            I[0] = new Point(x, y);
+            y--;
+            x++;
             I[1] = new Point(x, y);
             int i = 0, j;
             boolean sama = false;
